b25c22f18d6d0153bbe8890e1e4cb5953a131a8d
Pawel Piecuch <pawel.piecuch@roche.com>
2019-10-14 11:55:53 +0200

    first commit with Godot module template

---
+++ register_types.cpp
@@ -0,0 +1,10 @@
+/* register_types.cpp */
+
+#include "register_types.h"
+
+void register_gd_unqlite_types() {
+}
+
+void unregister_gd_unqlite_types() {
+	//nothing to do here
+}
---
+++ register_types.h
@@ -0,0 +1,4 @@
+/* register_types.h */
+
+void register_gd_unqlite_types();
+void unregister_gd_unqlite_types();
